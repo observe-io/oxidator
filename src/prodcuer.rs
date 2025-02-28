4bbcc700a4c41b05d0a5f0fe93d6b665efaf9e50
Aki0x137 <akshay.developer.007@gmail.com>
2025-02-28 13:17:51 +0530

    feat(producer): implement producer

--- src/prodcuer.rs
+++ src/prodcuer.rs
@@ -11,7 +11,7 @@ struct MultiProducerSequencer {
 
 struct Producer<T, D: DataStorage<T>, S: Sequencer> {
     sequencer: S,
-    data_storage: D<T>,
+    data_storage: Arc<D<T>>,
 }
 
 impl<T, S: Sequencer, D: DataStorage<T>> Producer<T, S, D> {
@@ -25,22 +25,32 @@ impl<T, S: Sequencer, D: DataStorage<T>> Producer<T, S, D> {
 
 impl<T, S: Sequencer, D: DataStorage<T>> EventProducer for Producer<T, S, D> {
     type Event = T;
-    fn write<E, F>(&self, events: E, f: F)
+    fn write<E, F>(&self, events: E, func: F)
     where
         E: IntoIterator,
         F: Fn(&mut E, Sequence, &E)
     {
         // TODO:
         // 1. get items iterator
+        let items = events.into_iter();
         // 2. get start and end slots for available for these events from sequencer
+        let (start, end) = self.sequencer.next(items.size());
         // 3. loop through items
-        // 4. get data provider slot and write to it
+        for (i, item) in items.enumerate() {
+            // 4. get data provider slot and write to it
+            let sequence = start + i as Sequence;
+            unsafe {
+                let slot = &mut self.data_storage.get_data_mut(sequence);
+                func(slot, sequence, &item);
+            };
+        }
         // 5. exit loop
         // 6. publish on sequencer
+        self.sequencer.publish(start, end);
     }
 
     fn drain(&self) {
-        // drain sequencer
+        self.sequencer.drain();
     }
 }
 
